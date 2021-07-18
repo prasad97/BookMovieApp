97ffc658bf4adbeaa9ca938cd60499b6db204ac3
Prasad Sawant <prasad.sawant20397@gmail.com>
2021-07-18 22:13:23 +0530

    Upcoming Movies Header added

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -2,13 +2,18 @@
 import React, { Component } from 'react'
 import Header from '../../common/header/Header'
 
+// Styles
+import "./Home.css";
+
 export default class Home extends Component {
     render() {
         return (
             <div>
                 {/* Header */}
                 <Header  baseUrl={this.props.baseUrl}></Header>
-                
+                <div className='upcomingMoviesHeader'>
+                    Upcoming Movies
+                </div>
                 <h1>Home Content</h1>
             </div>
         )
