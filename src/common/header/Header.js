e78c7fb739d01362d1cbce9d9cef6332d42e01bf
Prasad Sawant <prasad.sawant20397@gmail.com>
2021-07-18 21:59:54 +0530

    register functionality added

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -49,6 +49,17 @@ export default class Header extends Component {
             usernameRequiredClass: 'none',
             password: '',
             passwordRequiredClass: 'none',
+            firstname: '',
+            firstnameRequiredClass: 'none',
+            lastname: '',
+            lastnameRequiredClass: 'none',
+            email: '',
+            emailRequiredClass: 'none',
+            registrationPassword:'',
+            registrationPasswordRequiredClass: 'none',
+            contact:'',
+            contactRequiredClass: 'none',
+            registeredSuccessfully: false,
         }
     }
 
@@ -74,13 +85,33 @@ export default class Header extends Component {
         this.setState({ password: event.target.value });
     }
 
+    changeFormFirstnameHandler = (event) => {
+        this.setState({ firstname: event.target.value });
+    }
+
+    changeFormLastnameHandler = (event) => {
+        this.setState({ lastname: event.target.value });
+    }
+
+    changeEmailHandler = (event) => {
+        this.setState({ email: event.target.value });
+    }
+
+    changeRegistrationPasswordHandler = (event) => {
+        this.setState({ registrationPassword: event.target.value });
+    }
+
+    changeContactHandler = (event) => {
+        this.setState({ contact: event.target.value });
+    }
+
     // Login / Logout handlers
 
-    loginHandler = async () =>{
+    loginHandler = async () => {
         // alert("Login function Called");
-        
+
         // validate data
-        this.state.username === '' ? this.setState({ usernameRequiredClass : 'block' }) :this.setState({ usernameRequiredClass : 'none' });
+        this.state.username === '' ? this.setState({ usernameRequiredClass: 'block' }) : this.setState({ usernameRequiredClass: 'none' });
         this.state.password === "" ? this.setState({ passwordRequiredClass: "block" }) : this.setState({ passwordRequiredClass: "none" });
 
         // return if blank username & password are passed
@@ -89,49 +120,96 @@ export default class Header extends Component {
         //login user
         // console.log(this.props.baseUrl);
 
-        let that = this;
         const url = this.props.baseUrl + "auth/login";
         // console.log(url);
         const params = window.btoa(this.state.username + ":" + this.state.password);
 
-        try{
-            const rawResponse = await fetch(url,{
-                method:'POST',
-                headers:{
+        try {
+            const rawResponse = await fetch(url, {
+                method: 'POST',
+                headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json;charset=UTF-8',
                     "Authorization": `Basic ${params}`,
                 }
             });
 
-            if(rawResponse.ok){
+            if (rawResponse.ok) {
                 const response = await rawResponse.json();
                 // console.log(response);
                 // window.location.href = "/";
-                window.sessionStorage.setItem('user-details',JSON.stringify(response));
-                window.sessionStorage.setItem('userId',JSON.stringify(response.id));
-                window.sessionStorage.setItem('access-token',rawResponse.headers.get('access-token'));
-                that.setState({isLoggedIn:true});
+                window.sessionStorage.setItem('user-details', JSON.stringify(response));
+                window.sessionStorage.setItem('userId', JSON.stringify(response.id));
+                window.sessionStorage.setItem('access-token', rawResponse.headers.get('access-token'));
+                this.setState({ isLoggedIn: true });
                 // console.log(that.state.isLoggedIn);
-                that.closeModalHandler();
+                this.closeModalHandler();
             }
-            else{
+            else {
                 const error = new Error();
                 error.message = 'Something went wrong.';
                 throw error;
             }
-        } catch(e){
+        } catch (e) {
             alert(e.message);
         }
-        
-        
+
+
     }
 
     logoutHandler = () => {
         sessionStorage.removeItem('user-details');
         sessionStorage.removeItem('userId');
         sessionStorage.removeItem('access-token');
-        this.setState({isLoggedIn:false});
+        this.setState({ isLoggedIn: false });
+    }
+
+    // Register Handler
+    registerHandler = async ()=> {
+
+        // Validation
+        this.state.firstname === ""? this.setState({firstnameRequiredClass:'block'}) : this.setState({firstnameRequiredClass:'none'});
+        this.state.lastname === ""? this.setState({lastnameRequiredClass:'block'}) : this.setState({lastnameRequiredClass:'none'});
+        this.state.email === ""? this.setState({emailRequiredClass:'block'}) : this.setState({emailRequiredClass:'none'});
+        this.state.registrationPassword === ""? this.setState({registrationPasswordRequiredClass:'block'}) : this.setState({registrationPasswordRequiredClass:'none'});
+        this.state.contact === ""? this.setState({contactRequiredClass:'block'}) : this.setState({contactRequiredClass:'none'});
+
+        if(this.state.firstname === "" || this.state.lastname === "" || this.state.email === "" || this.state.registrationPassword === "" || this.state.contact === ""){
+            return;
+        }
+
+        let data = {
+            "email_address": this.state.email,
+            "first_name": this.state.firstname,
+            "last_name": this.state.lastname,
+            "mobile_number": this.state.contact,
+            "password": this.state.registrationPassword,
+          }
+
+        const url = this.props.baseUrl + "signup";
+        
+        try{
+            // debugger;
+            const rawResponse = await fetch(url,{
+                method:'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json;charset=UTF-8',
+                },
+                body: JSON.stringify(data)
+            });
+
+            
+            if(rawResponse.ok){
+                // const response = await rawResponse.json();
+                // console.log(response);
+                this.setState({registeredSuccessfully : true});
+            }
+        }catch(e){
+            alert(e.message);
+        }
+        
+        
     }
 
     render() {
@@ -203,7 +281,7 @@ export default class Header extends Component {
                                     </span>
                                 </FormHelperText>
                             </FormControl>
-                            <br/><br/>
+                            <br /><br />
                             <FormControl required>
                                 <InputLabel htmlFor="password">Password</InputLabel>
                                 <Input type="password" id="password" onChange={this.changeFormPasswordHandler} />
@@ -216,7 +294,69 @@ export default class Header extends Component {
                             <br /><br />
                             <Button variant="contained" color="primary" onClick={this.loginHandler}>LOGIN</Button>
                         </TabContainer>}
-                    
+                    {this.state.value === 1 &&
+                        <TabContainer>
+                            <FormControl required>
+                                <InputLabel htmlFor="firstname">First Name</InputLabel>
+                                <Input type="text" id="firstname" onChange={this.changeFormFirstnameHandler} />
+                                <FormHelperText className={this.state.firstnameRequiredClass}>
+                                    <span className='red'>
+                                        required
+                                    </span>
+                                </FormHelperText>
+                            </FormControl>
+                            <br /><br />
+
+                            <FormControl required>
+                                <InputLabel htmlFor="lastname">Last Name</InputLabel>
+                                <Input type="text" id="lastname" onChange={this.changeFormLastnameHandler} />
+                                <FormHelperText className={this.state.lastnameRequiredClass}>
+                                    <span className='red'>
+                                        required
+                                    </span>
+                                </FormHelperText>
+                            </FormControl>
+                            <br /><br />
+
+                            <FormControl required>
+                                <InputLabel htmlFor="email">Email</InputLabel>
+                                <Input id="email" type="email" onChange={this.changeEmailHandler} />
+                                <FormHelperText className={this.state.emailRequiredClass}>
+                                    <span className="red">
+                                        required
+                                    </span>
+                                </FormHelperText>
+                            </FormControl><br /><br />
+
+                            <FormControl required>
+                                <InputLabel htmlFor="registrationPassword">Password</InputLabel>
+                                <Input type="password" id="registrationPassword" onChange={this.changeRegistrationPasswordHandler} />
+                                <FormHelperText className={this.state.registrationPasswordRequiredClass}>
+                                    <span className="red">
+                                        required
+                                    </span>
+                                </FormHelperText>
+                            </FormControl><br /><br />
+
+                            <FormControl required>
+                                <InputLabel htmlFor="contact">Contact No</InputLabel>
+                                <Input id="contact" onChange={this.changeContactHandler} />
+                                <FormHelperText className={this.state.contactRequiredClass}>
+                                    <span className="red">
+                                        required
+                                    </span>
+                                </FormHelperText>
+                            </FormControl><br /><br />
+
+                            {this.state.registeredSuccessfully === true &&
+                                <FormControl>
+                                    <span>Registration Successful. Please Login!</span>
+                                </FormControl>}<br/><br />
+                            
+                            <Button variant="contained" color="primary"
+                            onClick={this.registerHandler}>REGISTER</Button>
+
+                        </TabContainer>}
                 </Modal>
 
 
