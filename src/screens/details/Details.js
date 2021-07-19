5a2aaebe3a2e637088eacfdab3da5dd2bbaad70e
Prasad Sawant <prasad.sawant20397@gmail.com>
2021-07-19 17:32:05 +0530

    details page left section added

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -11,23 +11,64 @@ import './Details.css';
 export default class Details extends Component {
     constructor() {
         super();
-        this.state = {}
+        this.state = {
+            movie : {
+
+            }
+        }
     }
 
-    
+    // Get Movie Detail
+    getSelectedMovie = async (queryString) => {
+        const url = this.props.baseUrl + 'movies/' + this.props.match.params.id;
+        // console.log(url);
+
+        try {
+            const rawResponse = await fetch(url);
+
+            if (rawResponse.ok) {
+                const response = await rawResponse.json();
+                // console.log(response);
+                this.setState({ movie: response});
+            }
+            else {
+                const error = new Error();
+                error.message = "Some Error Occurred";
+                throw error;
+            }
+        } catch (e) {
+            alert(e.message);
+        }
+    }
+
+    componentWillMount(){
+        this.getSelectedMovie();
+    }
 
     render() {
         return (
             <div>
-                <Header baseUrl={this.props.baseUrl} showBookButton="true" />
+                <Header baseUrl={this.props.baseUrl} showBookButton="true"  id={this.props.match.params.id} />
                 <div className="back-btn">
                     <Typography>
                       <Link to="/">  &#60; Back to Home</Link>
                     </Typography>
                 </div>
 
-                <h1>Detail Screen here</h1>
+                <div className="container">
+                    <div className="left">
+                        <img src={this.state.movie.poster_url} alt={this.state.movie.title}/>
+                    </div>
+                    <div className="middle">
+
+                    </div>
+                    <div className="left">
+
+                    </div>
+                </div>
+                
             </div>
+
         )
     }
 }
